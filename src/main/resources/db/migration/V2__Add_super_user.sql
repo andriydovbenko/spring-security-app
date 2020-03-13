18863dc84a2da1c3cc87c94c907be69159324f12
andriydovbenko <andriydovbenko1@gmail.com>
2020-03-13 07:47:31 +0100

    fixed problem with creating firs user

--- src/main/resources/db/migration/V2__Add_super_user.sql
+++ src/main/resources/db/migration/V2__Add_super_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO pajama.public.usr (id, active, password, username) VALUES (1,true, '1111', 'admin');
-INSERT INTO pajama.public.user_role (user_id, roles) VALUES (1,'ADMIN');
+INSERT INTO pajama.public.usr (id, active, password, username) VALUES (0,true, '1111', 'admin');
+INSERT INTO pajama.public.user_role (user_id, roles) VALUES (0,'ADMIN');
